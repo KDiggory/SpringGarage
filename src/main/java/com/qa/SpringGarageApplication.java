02aebe563df9b77bd687dc99bee2f671da81ce4b
KDiggory <katie.diggory@gmail.com>
2021-10-08 13:39:30 +0100

    Initial commit

---
+++ src/main/java/com/qa/SpringGarageApplication.java
@@ -0,0 +1,14 @@
+package com.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGarageApplication.class, args);
+	System.out.println("I am running!");
+	}
+
+}
